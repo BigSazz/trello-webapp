d68927cf1c8aca88bcb25ead30528ea4856592a2
Osaze Oviawe <ocubed88@gmail.com>
2023-11-16 17:36:39 +0100

    feat: - added functionality for creating an organization after log in

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,53 @@
+import { Plus } from "lucide-react";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+
+export const Navbar = () => {
+	return (
+		<nav className="fixed z-50 px-4 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+			<div className="flex items-center gap-x-4">
+				<div className="hidden md:flex">
+					<Logo />
+				</div>
+				<Button
+					variant="primary"
+					size="sm"
+					className="rounded-sm h-auto py-1.5 px-2"
+				>
+					<Plus className="block md:hidden h-4 w-4" />
+					<span className="hidden md:block">Create</span>
+				</Button>
+			</div>
+			<div className="ml-auto flex items-center gap-x-2">
+				<OrganizationSwitcher
+					hidePersonal
+					afterCreateOrganizationUrl="/organization/:id"
+					afterSelectOrganizationUrl="/organization/:id"
+					afterLeaveOrganizationUrl="/select-org"
+					appearance={{
+						elements: {
+							rootBox: {
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+							},
+						},
+					}}
+				/>
+				<UserButton
+					afterSignOutUrl="/"
+					appearance={{
+						elements: {
+							avatarBox: {
+								height: 30,
+								width: 30,
+							},
+						},
+					}}
+				/>
+			</div>
+		</nav>
+	);
+};
